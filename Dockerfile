744c5afcacc2f7636fa406ed6c59632a0f8a8cb7
yefry.gutierrez <yefrygf@gmail.com>
2025-03-18 16:15:04 -0500

    update urls

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY build/libs/payrll-0.0.1-SNAPSHOT.jar app.jar
 
 # Expose the application port (default Spring Boot port)
-EXPOSE 8080
+EXPOSE 8092
 
 # Command to run the application
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/maxiaseo/payrll/adapters/driven/feign/client/IFileClientServices.java
+++ src/main/java/com/maxiaseo/payrll/adapters/driven/feign/client/IFileClientServices.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "file-service", url = "http://localhost:8090/api/file")
+@FeignClient(name = "file-service", url = "${file-services.url}")
 @Component
 public interface IFileClientServices {
 
