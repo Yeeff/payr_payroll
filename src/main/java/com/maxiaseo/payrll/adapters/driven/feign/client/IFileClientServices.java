64979abb468f266d90ce33542a5980564c01ae3d
BaseGov <89814228+BaseGov@users.noreply.github.com>
2025-03-23 17:14:25 -0500

    Updated urls variables

--- src/main/java/com/maxiaseo/payrll/adapters/driven/feign/client/IFileClientServices.java
+++ src/main/java/com/maxiaseo/payrll/adapters/driven/feign/client/IFileClientServices.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "file-service", url = "${file-services.url}")
+@FeignClient(name = "file-service", url = "${file-services.url}/api/file")
 @Component
 public interface IFileClientServices {
 
