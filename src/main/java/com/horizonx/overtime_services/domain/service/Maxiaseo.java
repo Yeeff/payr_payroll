af048c5d6b90458f251afa01061f1a1b6a7d3d59
BaseGov <89814228+BaseGov@users.noreply.github.com>
2025-07-17 17:41:29 -0500

    Maxiaseo filters added

--- src/main/java/com/horizonx/overtime_services/domain/service/Maxiaseo.java
+++ src/main/java/com/horizonx/overtime_services/domain/service/Maxiaseo.java
@@ -2,17 +2,27 @@ package com.horizonx.overtime_services.domain.service;
 
 import com.horizonx.overtime_services.domain.util.TimeRange;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 
 public class Maxiaseo {
 
     public TimeRange validateLunchHour(TimeRange timeRange){
+
+        DayOfWeek day = timeRange.getStartTime().getDayOfWeek();
+
+        boolean isSunday = day == DayOfWeek.SUNDAY;
         Long hoursWorkedPerDay = Duration.between(
                 timeRange.getStartTime(), timeRange.getEndTime() ).toHours();
 
-        if( hoursWorkedPerDay != 12 ){
-            timeRange.setEndTime(timeRange.getEndTime().minusHours(1));
+        if( hoursWorkedPerDay == 12
+                || ( hoursWorkedPerDay < 9 && isSunday)
+                || (timeRange.getStartTime().getHour() == 10 && timeRange.getEndTime().getHour() == 20 )
+        ){
+            return timeRange;
         }
+
+        timeRange.setEndTime(timeRange.getEndTime().minusHours(1));
 
         return timeRange;
     }
